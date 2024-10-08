9303452542ee39ade36ca2de6bed92ac72c05d5c
Shriram33 <131244637+Shriram33@users.noreply.github.com>
2024-10-08 11:59:41 +0530

    new

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull shriram333/python-webapp
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 shriram333/python-webapp
