ee93fbf86ef9a9991e7e7dd7c5d56b85a1ceb0df
Shriram33 <131244637+Shriram33@users.noreply.github.com>
2025-05-13 21:05:00 +0530

    Delete scripts directory

--- scripts/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull shriram333/python-webapp
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 shriram333/python-webapp
